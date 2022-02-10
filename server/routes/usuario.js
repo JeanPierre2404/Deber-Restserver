eba4f2cf218ab14aca61afb5d2b6878c8a18da03
Casa JeanPierre <jean05343@gmail.com>
2022-02-10 18:27:11 -0500

    Agregando Peticiones PUT y GET

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 //Importar el Shema del usuario
 const Usuario = require('../models/usuario')
-
+    //importar undescore
+const _ = require('underscore');
 //Crear el objeto app
 const app = express();
 
@@ -12,7 +13,32 @@ const app = express();
 //Configurar los endpoint para usuarios
 app.get('/usuario', (req, res) => {
     /** CONSULTAR REGISTROS */
-    res.json('get Usuario');
+    //res.json('get Usuario');
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({})
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            Usuario.count({}, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    registros: conteo,
+                    usuarios
+                });
+            });
+
+        });
 });
 
 app.post('/usuario', (req, res) => {
@@ -45,11 +71,26 @@ app.post('/usuario', (req, res) => {
 });
 
 app.put('/usuario/:id', (req, res) => {
+    let id = rew.params.id;
     /** ACTUALIZAR REGISTROS */
     //res.json('put Usuario');
-    let body = req.body;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+    //delete body.password;
+    //delete body.google;
 
-    if (body.nombre === undefined) {
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+    });
+    res.json({
+        ok: true,
+        usuario: usuarioDB
+    });
+    /*if (body.nombre === undefined) {
         res.status(400), json({
             mensaje: "El nombre es necesario"
         });
@@ -57,7 +98,7 @@ app.put('/usuario/:id', (req, res) => {
         res.json({
             persona: body
         });
-    }
+    }*/
 
 });
 
